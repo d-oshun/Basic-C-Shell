62fc6ab5feaf067bf49861bbecc4c17efd58602d
Diana Williams Oshun <145607859+d-oshun@users.noreply.github.com>
2024-08-26 17:21:06 +0100

    Add files via upload

---
+++ batchfile.sh
@@ -0,0 +1,22 @@
+# /*My name is Diana Williams Oshun, my student number is 23101961. I acknowlege the DCU Academic 
+# Integrity Policy*/
+
+ls -l  #lists out the files in the current folder
+pause  #pauses the batchfile so that the information can be viewed
+help   #displays the user help manual
+pause
+echo I am running purely on coffee #echos the words back to the user
+pause
+environ #outputs all the enviroment strings
+pause
+clr     #clear the screen 
+pause
+cd      #displays the current directory and if cd ./folder_name it changes the directory
+pause
+fork    #the fork command creates a child process
+pause
+exec ls #replaces the command running o n the child process
+pause
+quit    #exits the programme
+;
+
